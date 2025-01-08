a93eaf58f8971c0e70bc8982faa6ddf9832b4765
sriharshitha0117 <2311cs010503@mallareddyuniversity.ac.in>
2025-01-08 11:30:10 +0530

    Add files via upload
    problems

---
+++ Day_4.java
@@ -0,0 +1,11 @@
+public class Day_4 {
+    public static void main(String[] args){
+        for(int i=1;i<4;i++){
+        for(int j=1;j<=i;j++){
+            System.out.print("a");
+        }
+        System.out.println();
+        }
+    }
+    
+}
